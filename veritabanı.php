ea1e163ac53d32f75c5f03055343e5dce22136a6
Alradyin <alradyin@example.com>
2025-05-28 22:31:07 +0300

    veritabanı.php gizlendi

--- veritabanı.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $baglanti = new PDO("mysql:host=localhost;dbname=test;charset=utf8", "root", "");
-    $baglanti->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Veritabanı bağlantı hatası: " . $e->getMessage());
-}
-?>
-
